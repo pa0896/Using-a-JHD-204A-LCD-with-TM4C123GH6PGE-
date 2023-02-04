4690578bb954d6007fcde58b341fc6afbc278644
pa0896 <prashantagrawal0896@gmail.com>
2023-02-05 01:59:34 +0900

    zweite commit

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 
-#include <stdint.h>
 #define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
-#define GPIO_PORTA_DATA_R		(*((volatile unsigned long *)0x40004000))
-#define GPIO_PORTB_DATA_R       (*((volatile unsigned long *)0x40005000))
+#define GPIO_PORTA_DATA_R		(*((volatile unsigned long *)0x400043FC))
+#define GPIO_PORTB_DATA_R       (*((volatile unsigned long *)0x400053FC))
 #define GPIO_PORTA_DIR_R		(*((volatile unsigned long *)0x40004400))
 #define GPIO_PORTB_DIR_R        (*((volatile unsigned long *)0x40005400))
 #define GPIO_PORTA_DEN_R		(*((volatile unsigned long *)0x4000451C))
@@ -23,39 +22,40 @@
 #define Set5x7FontSize    		0x20
 #define FirstRow          		0x80
 unsigned long delay;
-void LCD_CMD(unsigned char c);
+void LCD_CMD(int c);
 void init(void);
 void LCD_Datei_Schreib(unsigned char Datei);
-void Aufschiebung_ms(uint16_t zeit);
-void Aufschiebung_us(uint8_t zeit);
+void Aufschiebung_ms(int zeit);
+void Aufschiebung_us(int zeit);
 
 void init(void){
-	SYSCTL_RCGC2_R |= 0x00000003;   //fängt die Uhr der beiden Port A und B
+	SYSCTL_RCGC2_R |= 0x00000001;   //fängt die Uhr der beiden Port A und B
+	SYSCTL_RCGC2_R |= 0x00000002;
 	delay = SYSCTL_RCGC2_R;         //warte auf die Uhr, zu starten
 	GPIO_PORTA_DIR_R |= 0x0000001C; //verwend bloß 2,3,4 pins
 	GPIO_PORTB_DIR_R |= 0x000000FF; //verwend allen Pins
 	GPIO_PORTA_DEN_R |= 0x0000001C;
 	GPIO_PORTB_DEN_R |= 0x000000FF;
 	LCD_CMD(0x20);   //verwend ein Display von 5 pixel länge und 7 pixel breit
-	LCD_CMD(0x38);   //wähle die 4-bit Einstellung
+	LCD_CMD(0x38);   //wähle die 8-bit Einstellung
 	LCD_CMD(0x01);   //räume das Display
 	LCD_CMD(0x0F);	 //cursor zwinkerung
 }
 
 void LCD_Datei_Schreib(unsigned char Datei){
-	GPIO_PORTA_DATA_R = 0x00000004;
+	GPIO_PORTA_DATA_R |= 0x00000004;  //RS = 1
 	GPIO_PORTB_DATA_R = Datei;
 	GPIO_PORTA_DATA_R |= 0x00000010;
 	GPIO_PORTA_DATA_R = 0x00000000;
 	Aufschiebung_us(0);
 }
 
-void LCD_CMD(unsigned char command){
-	GPIO_PORTA_DATA_R = 0x00000000;
+void LCD_CMD(int command){
+	GPIO_PORTA_DATA_R &= ~0x0000001C;  //RS = 0, R/W = 0, E = 0
 	GPIO_PORTB_DATA_R = command;
-	GPIO_PORTA_DATA_R = 0x00000010;
+	GPIO_PORTA_DATA_R |= 0x00000010;
 	Aufschiebung_us(0);
-	GPIO_PORTA_DATA_R = 0x00000000;
+	GPIO_PORTA_DATA_R &= ~0x00000010;
 	if(command < 4){
 		Aufschiebung_ms(2);
 	}
@@ -65,7 +65,7 @@ void LCD_CMD(unsigned char command){
 }
 
 
-void Aufschiebung_ms(uint16_t zeit){
+void Aufschiebung_ms(int zeit){
 	int i,j;
 	for(i = 0 ; i < zeit ; i++){
 		for(j = 0 ; j < 3180 ; j++){
@@ -74,9 +74,9 @@ void Aufschiebung_ms(uint16_t zeit){
 	}
 }
 
-void Aufschiebung_us(uint8_t zeit){
+void Aufschiebung_us(int zeit){
 	int k,l;
-	for(k = 0 ; k < zeit ; k++){
+	for(k = 0 ; k <= zeit ; k++){
 		for(l = 0 ; l < 3 ; l++){
 			// verschwinde zeit
 		}
@@ -89,8 +89,10 @@ int main(void)
 {
 	init();
 	while(1){
+		LCD_CMD(0x00001DCB);
 		LCD_CMD(0x01);
 		LCD_CMD(0x80);
+		LCD_CMD(0x38);
 		Aufschiebung_ms(500);
 		LCD_Datei_Schreib('P');
 		Aufschiebung_ms(1);
